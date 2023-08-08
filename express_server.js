987c156b39f41075fe85c7dc58d469a935b875ac
Difan <dfhou29@gmail.com>
2023-08-08 00:22:11 -0700

    post route added for /urls to receive request body users sent in the /urls/new

--- express_server.js
+++ express_server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const morgan = require('morgan');
 const PORT = 8080;
 
 const app = express();
@@ -9,7 +9,21 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// generate a random string with length of 6
+function generateRandomString() {
+  const alphabets = 'abcdefghijklmnopqrstuvwxyz';
+  let randomString = '';
+  for (let x = 0; x < 6; x++) {
+    randomString += alphabets[Math.floor(Math.random() * (25 + 1))];
+  }
+  return randomString;
+}
+
 app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send('Hello');
@@ -20,6 +34,15 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render('urls_new');
+})
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send('OK');
+})
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
@@ -28,10 +51,11 @@ app.get("/urls/:id", (req, res) => {
   res.render('urls_show', templateVars);
 })
 
-app.get("/hello", (req, res) => {
-  const templateVar = {greeting: 'Hello world!'};
-  res.render('hello_world', templateVar)  ;
-});
+
+// app.get("/hello", (req, res) => {
+//   const templateVar = {greeting: 'Hello world!'};
+//   res.render('hello_world', templateVar)  ;
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
