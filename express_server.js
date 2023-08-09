58ca809eea58809c149a8fbf1ccf3fa1efa38481
Difan <dfhou29@gmail.com>
2023-08-08 17:09:23 -0700

    delete button added for /urls. POST route added for handling DELETE request

--- express_server.js
+++ express_server.js
@@ -61,6 +61,11 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+})
+
 
 // app.get("/hello", (req, res) => {
 //   const templateVar = {greeting: 'Hello world!'};
