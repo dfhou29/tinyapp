6c5523a13f29d27a34fb124988e474dc7c64a336
Difan <dfhou29@gmail.com>
2023-08-08 20:55:22 -0700

    Modify _header.ejs to display a username using cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+
 const PORT = 8080;
 
 const app = express();
@@ -22,21 +24,45 @@ function generateRandomString() {
 app.set('view engine', 'ejs');
 
 app.use(morgan('dev'));
-
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send('Hello');
-});
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
+  console.log('cookie-username:', req.cookies['username']);
+  res.redirect("/urls");
+})
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
+
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies['username']
+  };
   res.render('urls_index', templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render('urls_new');
+  const templateVars = {
+    username: req.cookies['username']
+  }
+  res.render('urls_new', templateVars);
 })
+
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies['username']
+  }
+  res.render('urls_show', templateVars);
+})
+
+// app.get("/u/:id", (req, res) => {
+//   const longURL = urlDatabase[req.params.id];
+//   res.redirect(longURL);
+// });
 
 app.post("/urls", (req, res) => {
   console.log(req.body);
@@ -48,23 +74,6 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 })
 
-app.get("/urls/:id", (req, res) => {
-  const templateVars = {
-    id: req.params.id,
-    longURL: urlDatabase[req.params.id]
-  }
-  res.render('urls_show', templateVars);
-})
-
-// app.get("/u/:id", (req, res) => {
-//   const longURL = urlDatabase[req.params.id];
-//   res.redirect(longURL);
-// });
-
-app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect("/urls");
-})
 
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
